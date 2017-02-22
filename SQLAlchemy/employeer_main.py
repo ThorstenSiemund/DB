122bc14c0cf2da02f67ee96238cb9047ff0f0dab
SiemThor <thorsten.siemund@siemens-ppal.com>
2017-02-22 16:23:08 +0900

    added option --init_db bug fix: setting of department in employee

--- SQLAlchemy/employeer_main.py
+++ SQLAlchemy/employeer_main.py
@@ -3,6 +3,19 @@ from sqlalchemy.orm import sessionmaker
 from employeer_model import Base, Employee, Department
 from csv import DictReader
 from time import time
+from sys import exit
+import argparse
+
+
+def delete_all_tables(session):
+    print('deleting table "departments"')
+    for department in session.query(Department).all():
+        session.delete(department)
+
+    print('deleting table "employees"')
+    for employee in session.query(Employee).all():
+        session.delete(employee)
+    session.commit()
 
 
 def fill_with_testdata(session):
@@ -22,23 +35,23 @@ def fill_with_testdata(session):
         for row in reader:
             if row['Department'] in dict_of_departments:
                 dept = dict_of_departments[row['Department']]
-                # session.commit()
             else:
                 dept = Department(name=row['Department'])
                 dict_of_departments[row['Department']] = dept
-            print(dept)
-            session.add(dept)
 
             employee = Employee(firstname=row['Firstname'],
                                 lastname=row['Lastname'],
                                 hired_on=row['Hired'],
-                                department_id=dept,)
-            employee.department_id.append(dept.id)
+                                department=dept,)
             # if session.query(Employee).filter(and_(Employee.firstname == employee.firstname), Employee.lastname == employee.lastname).count() > 0:
             #     print('exists: %s' % employee)
+            session.add(dept)
             session.add(employee)
     print('Runtime: ', time() - fill_start_tm)
+    commit_start_tm = time()
     session.commit()
+    print('Runtime: ', time() - commit_start_tm)
+
 
 
 def create_session():
@@ -72,8 +85,32 @@ def main():
     Main part of the programm
     '''
     session = create_session()
-    fill_with_testdata(session)
+
+    # fill db with test data?
+    if results.init_db:
+        answer = input('If you realy sure to delete the whole DB, type "YES": ')
+        if answer == 'YES':
+            delete_all_tables(session)
+        exit()
+        print('-' * 100)
+        fill_with_testdata(session)
+
+    print('\nEmployees whose firstnames start with "C":')
+    for employee in session.query(Employee).filter(Employee.firstname.startswith("C")):
+        print(employee)
+
+    print('\nEmployees whose lastname is "Braun":')
+    for employee in session.query(Employee).filter(Employee.lastname == 'Braun'):
+        print(employee, employee.department)
+
+    print('\nEmployees whose departement is "Sales":')
+    for employee in session.query(Employee).join(Employee.department).filter(Department.name == 'Sales'):
+        print(employee, employee.department)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(allow_abbrev=False, description='Program for playing around with Sqlite and SQLAlchemy.')
+    parser.add_argument('--init_db', action="store_true", dest='init_db', default=False, help='fill DB with fake employees')
+    results = parser.parse_args()
+    print(results.init_db)
     main()
